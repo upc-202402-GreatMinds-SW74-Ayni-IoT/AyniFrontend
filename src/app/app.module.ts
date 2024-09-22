9dd841f658253dddec6a917acd5fa3333cdd50ab
jhonelrios <riosjaimes.j@gmail.com>
2024-09-21 21:59:23 -0500

    feat: navigate to crop detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,91 +1,80 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatStepperModule} from "@angular/material/stepper";
-import {CropsStepperContentComponent} from "./management/components/stepper-content/stepper-content.component";
-import {HttpClientModule} from "@angular/common/http";
-import {NgOptimizedImage} from "@angular/common";
-import {OverlayModule} from "@angular/cdk/overlay";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {
-  CropsStep1CardContentComponent
-} from './management/components/step1-card-content/step1-card-content.component';
-import {
-  CropsStep2CardContentComponent
-} from './management/components/step2-card-content/step2-card-content.component';
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
-import {
-  CropsStep3CardContentComponent
-} from './management/components/step3-card-content/step3-card-content.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import {CropsDialogContentComponent} from './management/components/dialog-content/dialog-content.component';
-import {SalesStepperContentComponent} from "./shopping/components/stepper-content/stepper-content.component";
-import {SalesStep1CardContentComponent} from './shopping/components/step1-card-content/step1-card-content.component';
-import {SalesStep2CardContentComponent} from './shopping/components/step2-card-content/step2-card-content.component';
-import {SalesStep3CardContentComponent} from './shopping/components/step3-card-content/step3-card-content.component';
-import {SaleDialogContentComponent} from './shopping/components/sale-dialog-content/dialog-content.component';
-import {OrdersService} from "./shopping/services/orders.service";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatDividerModule} from '@angular/material/divider';
-import {ButtonContentComponent} from './finance/components/cost-button-content/button-content.component';
-import {Button2ContentComponent} from './finance/components/profit-button-content/button2-content.component';
-import {TransactionsListContentComponent} from './finance/pages/transactions-list-content/transactions-list-content.component';
-import {NewTransactionDialogContentComponent} from './finance/components/new-transaction-dialog-content/new-transaction-dialog-content.component';
-import {CostsGalleryContentComponent} from './finance/pages/costs-gallery-content/costs-gallery-content.component';
-import {MatSelectModule} from "@angular/material/select";
-import {MatRadioModule} from '@angular/material/radio';
-import {MatTableModule} from "@angular/material/table";
-import {MatMenuModule} from "@angular/material/menu";
-import {ProfitsGalleryContentComponent} from './finance/pages/profits-gallery-content/profits-gallery-content.component';
-import {FarmerNavigationComponent} from './shared/components/farmer-navigation/farmer-navigation.component';
-import {SignInComponent} from './authentication/components/sign-in/sign-in.component';
-import {SignUpComponent} from './authentication/components/sign-up/sign-up.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatStepperModule } from '@angular/material/stepper';
+import { CropsStepperContentComponent } from './management/components/stepper-content/stepper-content.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CropsStep1CardContentComponent } from './management/components/step1-card-content/step1-card-content.component';
+import { CropsStep2CardContentComponent } from './management/components/step2-card-content/step2-card-content.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CropsStep3CardContentComponent } from './management/components/step3-card-content/step3-card-content.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CropsDialogContentComponent } from './management/components/dialog-content/dialog-content.component';
+import { SalesStepperContentComponent } from './shopping/components/stepper-content/stepper-content.component';
+import { SalesStep1CardContentComponent } from './shopping/components/step1-card-content/step1-card-content.component';
+import { SalesStep2CardContentComponent } from './shopping/components/step2-card-content/step2-card-content.component';
+import { SalesStep3CardContentComponent } from './shopping/components/step3-card-content/step3-card-content.component';
+import { SaleDialogContentComponent } from './shopping/components/sale-dialog-content/dialog-content.component';
+import { OrdersService } from './shopping/services/orders.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { ButtonContentComponent } from './finance/components/cost-button-content/button-content.component';
+import { Button2ContentComponent } from './finance/components/profit-button-content/button2-content.component';
+import { TransactionsListContentComponent } from './finance/pages/transactions-list-content/transactions-list-content.component';
+import { NewTransactionDialogContentComponent } from './finance/components/new-transaction-dialog-content/new-transaction-dialog-content.component';
+import { CostsGalleryContentComponent } from './finance/pages/costs-gallery-content/costs-gallery-content.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { ProfitsGalleryContentComponent } from './finance/pages/profits-gallery-content/profits-gallery-content.component';
+import { FarmerNavigationComponent } from './shared/components/farmer-navigation/farmer-navigation.component';
+import { SignInComponent } from './authentication/components/sign-in/sign-in.component';
+import { SignUpComponent } from './authentication/components/sign-up/sign-up.component';
 
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatCardModule} from "@angular/material/card";
-import {MatListModule} from "@angular/material/list";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {SelectRolComponent} from './authentication/components/select-rol/select-rol.component';
-import {
-  FarmerMainContentComponent
-} from './authentication/components/home/farmer-home/components/farmer-main-content/farmer-main-content.component';
-import {
-  MerchantMainContentComponent
-} from './authentication/components/home/merchant-home/components/merchant-main-content/merchant-main-content.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {
-  ProductInformationContentComponent
-} from "./management/pages/product-information-content/product-information-content.component";
-import {ProductsGalleryContentComponent} from "./management/pages/products-gallery-content/products-gallery-content.component";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {ProductCardContentComponent} from "./management/components/product-card-content/product-card-content.component";
-import {MatPaginatorModule} from "@angular/material/paginator";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { SelectRolComponent } from './authentication/components/select-rol/select-rol.component';
+import { FarmerMainContentComponent } from './authentication/components/home/farmer-home/components/farmer-main-content/farmer-main-content.component';
+import { MerchantMainContentComponent } from './authentication/components/home/merchant-home/components/merchant-main-content/merchant-main-content.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProductInformationContentComponent } from './management/pages/product-information-content/product-information-content.component';
+import { ProductsGalleryContentComponent } from './management/pages/products-gallery-content/products-gallery-content.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductCardContentComponent } from './management/components/product-card-content/product-card-content.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { OrderCardContentComponent } from './shopping/components/order-card-content/order-card-content.component';
 import { OrdersGalleryContentComponent } from './shopping/pages/orders-gallery-content/orders-gallery-content.component';
 import { CancelPurchaseDialogContentComponent } from './shopping/components/cancel-purchase-dialog-content/cancel-purchase-dialog-content.component';
 import { OrderInformationContentComponent } from './shopping/pages/order-Information-content/order-information-content.component';
 import { NewProductFormContentComponent } from './management/components/new-product-form-content/new-product-form-content.component';
-import {RatesGalleryContentComponent} from "./shopping/pages/rates-gallery-content/rates-gallery-content.component";
+import { RatesGalleryContentComponent } from './shopping/pages/rates-gallery-content/rates-gallery-content.component';
 import { RateCardContentComponent } from './shopping/components/rate-card-content/rate-card-content.component';
 import { SaleCardContentComponent } from './shopping/components/sale-card-content/sale-card-content.component';
 import { SalesGalleryContentComponent } from './shopping/pages/sales-gallery-content/sales-gallery-content.component';
 import { NewSaleFormContentComponent } from './shopping/components/new-sale-form-content/new-sale-form-content.component';
 import { SaleInformationContentComponent } from './shopping/pages/sale-information-content/sale-information-content.component';
-import { OrderRequestsContentComponent } from "./shopping/pages/order-requests-content/order-requests-content.component";
+import { OrderRequestsContentComponent } from './shopping/pages/order-requests-content/order-requests-content.component';
 import { MerchantNavigationComponent } from './shared/components/merchant-navigation/merchant-navigation.component';
-import {OrderDialogContentComponent} from "./shopping/components/order-dialog-content/order-dialog-content.component";
-import {RateDialogContentComponent} from "./shopping/components/rate-dialog-content/rate-dialog-content.component";
-import {MatSortModule} from "@angular/material/sort";
-import {authInterceptorProviders} from "./authentication/helpers/AuthInterceptor";
+import { OrderDialogContentComponent } from './shopping/components/order-dialog-content/order-dialog-content.component';
+import { RateDialogContentComponent } from './shopping/components/rate-dialog-content/rate-dialog-content.component';
+import { MatSortModule } from '@angular/material/sort';
+import { authInterceptorProviders } from './authentication/helpers/AuthInterceptor';
 import { ContactDialogComponent } from './authentication/components/contact-dialog/contact-dialog.component';
 import { CropDetailsComponent } from './management/components/crop-details/crop-details.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -165,9 +154,9 @@ import { CropDetailsComponent } from './management/components/crop-details/crop-
     MatPaginatorModule,
     MatProgressBarModule,
     MatSortModule,
+    NgChartsModule,
   ],
   providers: [OrdersService, authInterceptorProviders],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/shared/components/merchant-navigation/merchant-navigation.component.ts
+++ src/app/shared/components/merchant-navigation/merchant-navigation.component.ts
@@ -1,34 +1,34 @@
-import {Component, HostListener} from '@angular/core';
-import {Router} from "@angular/router";
-import {TokenStorageService} from "../../../authentication/services/token-storage.service";
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../../../authentication/services/token-storage.service';
 
 @Component({
   selector: 'app-merchant-navigation',
   templateUrl: './merchant-navigation.component.html',
-  styleUrls: ['./merchant-navigation.component.css']
+  styleUrls: ['./merchant-navigation.component.css'],
 })
 export class MerchantNavigationComponent {
   user = this.tokenStorage.getUser();
   isLargeScreen = window.innerWidth >= 768;
 
-  routesMerchant: {label:string , to: string}[] = [
-    {label: "Home", to: "/merchant-home"},
-    {label: "Products", to: "/sales"},
-    {label: "My shopping", to: "/purchases"},
-    {label: "Rate", to: "/rates"},
-  ]
+  routesMerchant: { label: string; to: string }[] = [
+    { label: 'Home', to: '/merchant-home' },
+    { label: 'Products', to: '/sales' },
+    { label: 'My shopping', to: '/purchases' },
+    // { label: 'Rate', to: '/rates' },
+  ];
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any): void {
     this.isLargeScreen = event.target.innerWidth >= 768;
   }
-  constructor(private router: Router,
-              private tokenStorage: TokenStorageService) {
-  }
+  constructor(
+    private router: Router,
+    private tokenStorage: TokenStorageService
+  ) {}
   onLogOut() {
-    console.log("LogOUT");
+    console.log('LogOUT');
     this.tokenStorage.signOut();
-    this.router.navigate(["signin"]);
+    this.router.navigate(['signin']);
   }
-
 }
