15d6a209aade4157e56489d3b77f3af7aa803e90
Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
2024-09-25 20:37:47 -0500

    fix: update json placeholder

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // apiUrl: 'https://ayni-api-test.zeabur.app/api/v1',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://my-json-server.typicode.com/JorgeGonzales15/ayni-jsonplaceholder-iot',
 };
